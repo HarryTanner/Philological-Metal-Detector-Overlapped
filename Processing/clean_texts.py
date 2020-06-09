a63ce8cc7bde82422ba6dc1bf3e067f96218d9e2
HarryTanner <hrt15f@googlemail.com>
2020-06-09 12:03:45 +0100

    Text Files Generated for Training
    Note: very minimal stopword exclusion.
    The training files are not included in the commit, because they are too large for GitHub.

--- Processing/clean_texts.py
+++ Processing/clean_texts.py
@@ -3,8 +3,8 @@
 This script is used to build the initial Word2Vec models on each individual century, in order to provide a good model of whether Word2Vec could reliably be used
 for Greek on periods as small as a single century'''
 
-#Initialise list of centuries to process. This study only covers BCE.
-centuries=[-800,-700,-600,-500,-400,-300,-200,-100]
+#Initialise list of centuries to process. This study only covers BCE. This list does not include -500 because there is no data in Diorisis.
+centuries=[-800,-700,-600,-400,-300,-200,-100,0]
 #Initialise list of stopwords to be disincluded from Word2Vec
 stopwords=["article","particle","conjunction","preposition"]
 
@@ -12,7 +12,7 @@ from lxml import etree
 import json
 
 #The json_list location can be changed as appropriate
-def clean_century(century, json_list="../Output/rounded_dates.json", diorisis_location="../Input/Diorisis"):
+def clean_century(century, json_list="../JSON/rounded_dates.json", diorisis_location="../Diorisis"):
 	file_list=[]
 	with open(json_list, "r") as json_reader:
 		date_list=json.load(json_reader)
@@ -20,8 +20,8 @@ def clean_century(century, json_list="../Output/rounded_dates.json", diorisis_lo
 		if date_list[file]==century:
 			file_list.append(diorisis_location+"/"+file)
 	#File list of all files which contain the texts for the century has been built. Now build a nested list of sentences.
-	nested_century=[]
 	print("Reading Files from Diorisis and Concatenating.")
+	corpus_string=""
 	for file in file_list:
 		with open(file, "r") as xml_reader:
 			xml_file=xml_reader.read()
@@ -29,27 +29,29 @@ def clean_century(century, json_list="../Output/rounded_dates.json", diorisis_lo
 		text=etree.fromstring(xml_file, parser=parser)
 		sentences=text.iter("sentence")
 		for sentence in sentences:
-			nested_sentence=[]
+			nested_sentence=""
 			for word in sentence:
 				for lemma in word:
-					if lemma.get("entry")==None:
-						pass
 					if lemma.get("POS") not in stopwords:
-						import_lemma=lemma.get("entry")
-						nested_sentence.append(import_lemma)
-					else:
-						pass
-			nested_century.append(nested_sentence)
-	return nested_century
+						if not lemma.get("entry"):
+							pass
+						else:
+							import_lemma=lemma.get("entry")
+							if not nested_sentence:
+								nested_sentence=import_lemma
+							else:
+								nested_sentence=nested_sentence+" "+import_lemma
+			corpus_string=corpus_string+"\n"+nested_sentence
+	return corpus_string
 
-def store_json(nested_century, file_name, json_location="../Output/"):
-	with open(json_location+file_name,"w+") as json_writer:
-		#Preserve the Greek forms with ensure_ascii set to false
-		json.dump(nested_century, json_writer, ensure_ascii=False)
-	print("Stored as: "+str(file_name))
+def store_file(nested_century, file_name):
+	with open(file_name,"w+") as file_writer:
+		file_writer.write(nested_century)
+		file_writer.close()
 
 for century in centuries:
 	print("Cleaning: "+str(century))
-	print("Storing Nested Century as JSON in Output at ")
+	file_name="../Texts/"+str(century)+".text"
+	print("Storing Nested Century as text for training at "+file_name)
 	nested_century=clean_century(century)
-	store_json(nested_century, file_name=str(century)+"_concatenated.json")
+	store_file(nested_century, file_name)
